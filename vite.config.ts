3e8d7a0e2162474cbf2a6e0eaa9b1b8bdecde2f4
gabrielnasser01 <gabrielnasser38@gmail.com>
2026-02-18 16:14:45 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './frontend',
+  envDir: '../',
   server: {
     port: 3000,
     host: true,
